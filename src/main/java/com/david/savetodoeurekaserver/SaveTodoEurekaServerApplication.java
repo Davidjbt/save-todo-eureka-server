d27cbe8def2f66f7e97e30e614c99b95eda30215
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-06-20 14:27:32 +0100

    Initial commit

---
+++ src/main/java/com/david/savetodoeurekaserver/SaveTodoEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.david.savetodoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveTodoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveTodoEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/savetodoeurekaserver/SaveTodoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.savetodoeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTodoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
