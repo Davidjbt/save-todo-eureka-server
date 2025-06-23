07f5653f8c20ca45156b306fd22db9779962e6e5
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-06-23 12:35:55 +0100

    Make application act as a Eureka Discovery Server
    Added config for Eureka Discovery server

--- src/main/java/com/david/savetodoeurekaserver/SaveTodoEurekaServerApplication.java
+++ src/main/java/com/david/savetodoeurekaserver/SaveTodoEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.david.savetodoeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SaveTodoEurekaServerApplication {
 
 	public static void main(String[] args) {
